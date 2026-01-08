974694d83f54f02698589c76f32b324f40ed5464
htu6n7yi <jcavalcanti008@gmail.com>
2026-01-08 01:10:13 -0300

    fix: ajustes no layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import { h1, main } from "framer-motion/client";
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <h1>Welcome to the Gallery</h1>
-  );
+    <main>
+      galeira de fotos
+    </main>
+  )
 }
